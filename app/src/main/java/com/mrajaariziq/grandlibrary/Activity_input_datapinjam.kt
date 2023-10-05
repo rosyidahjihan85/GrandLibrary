0211d66ae658a06543c3eab080ebe6fe632aeffc
user <farhanevandhika>
2023-10-05 10:45:00 +0700

    activity_input_datapinjam.xml activity_beranda.xml

---
+++ app/src/main/java/com/mrajaariziq/grandlibrary/Activity_input_datapinjam.kt
@@ -0,0 +1,11 @@
+package com.mrajaariziq.grandlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_input_datapinjam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_datapinjam)
+    }
+}
