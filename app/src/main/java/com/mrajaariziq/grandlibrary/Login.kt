aeca820d6c8836117b4cfc5844e5defe74978073
erlinnazila <erlinnazila15@gmail.com>
2023-10-05 07:55:57 +0700

    new desain

--- app/src/main/java/com/mrajaariziq/grandlibrary/Login.kt
+++ app/src/main/java/com/mrajaariziq/grandlibrary/Login.kt
@@ -1,11 +1,20 @@
 package com.mrajaariziq.grandlibrary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mrajaariziq.grandlibrary.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun login (view: View){
+        val ini = Intent (this, Beranda::class.java)
+        startActivity(ini)
     }
 }
--- app/src/main/java/com/mrajaariziq/grandlibrary/MainActivity.kt
+++ app/src/main/java/com/mrajaariziq/grandlibrary/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Handler().postDelayed({
-            startActivity(Intent(this, Beranda::class.java))
+            startActivity(Intent(this, Login::class.java))
             finish()
         }, 2300)
     }
