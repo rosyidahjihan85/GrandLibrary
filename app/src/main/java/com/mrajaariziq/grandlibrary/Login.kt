8154c29011c61e4efbb25ca20d5909dbf460b699
erlinnazila <erlinnazila15@gmail.com>
2023-10-11 11:31:12 +0700

    update detail_adapter.xml

--- app/src/main/java/com/mrajaariziq/grandlibrary/Login.kt
+++ app/src/main/java/com/mrajaariziq/grandlibrary/Login.kt
@@ -4,18 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.mrajaariziq.grandlibrary.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
 
-    fun login (view: View){
-        val ini = Intent (this, Beranda::class.java)
-        startActivity(ini)
-    }
+
 }
