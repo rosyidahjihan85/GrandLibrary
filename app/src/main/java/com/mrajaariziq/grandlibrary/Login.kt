edba6baf90d4f247c19a060147346ecd9ea1f7c8
piki16 <vickyaulia@gmail.com>
2023-11-08 11:19:15 +0700

    login

--- app/src/main/java/com/mrajaariziq/grandlibrary/Login.kt
+++ app/src/main/java/com/mrajaariziq/grandlibrary/Login.kt
@@ -1,20 +1,44 @@
 package com.mrajaariziq.grandlibrary
 
-import android.content.Intent
+import     android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import com.mrajaariziq.grandlibrary.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val innama = binding.loginnama
+        val inpassword = binding.loginpassword
+        val password = "12345"
+        val user = listOf<String>("admin","peminjam")
+        binding.btnlogin.setOnClickListener{
+
+            if (innama.text.isNotEmpty()&& inpassword.text.isNotEmpty())
+
+                if (inpassword.text.length>= 5){
+
+                    startActivity(
+                        Intent(this,Beranda::class.java)
+                            .putExtra("username",innama.text.toString())
+                    )
+                    alert("Selamat Datang di Grand Library  ${innama.text}")
+
+                }else{
+                    alert("password minimal 5 huruf")
+                }
+        }
     }
-    fun login (view: View){
-        val ini = Intent (this, Beranda::class.java)
-        startActivity(ini)
-    }
+
+}
+private fun alert(msg:String){
+    Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
 }
