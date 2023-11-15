b8704a7616338837f3a80d4d1cff4f5b83e7651b
rzqzdn <rizqizidan26@gmail.com>
2023-11-15 11:38:29 +0700

    brnd & login

--- app/src/main/java/com/mrajaariziq/grandlibrary/Beranda.kt
+++ app/src/main/java/com/mrajaariziq/grandlibrary/Beranda.kt
@@ -1,14 +1,24 @@
 package com.mrajaariziq.grandlibrary
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.mrajaariziq.grandlibrary.databinding.ActivityBerandaBinding
 
 class Beranda : AppCompatActivity() {
+    private lateinit var binding: ActivityBerandaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_beranda)
+        binding= ActivityBerandaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sharedPreferences = getSharedPreferences("Grand",Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("username","")
+
+        binding.usernma.text= username
+
     }
     fun databuku (view: View){
         val bookdata = Intent (this, BookData::class.java)
